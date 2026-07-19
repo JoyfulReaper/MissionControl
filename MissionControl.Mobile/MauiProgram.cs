520916cdf1e711fb7256dfd6b68f7c58c71d9044
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-19 17:39:04 -0400

    feat(mobile): display live agent status

--- MissionControl.Mobile/MauiProgram.cs
+++ MissionControl.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MissionControl.Client.Agent;
 
 namespace MissionControl.Mobile;
 
@@ -7,18 +8,32 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont(
+                    "OpenSans-Regular.ttf",
+                    "OpenSansRegular");
             });
 
         builder.Services.AddMauiBlazorWebView();
 
+        builder.Services.AddHttpClient<
+            IAgentSnapshotClient,
+            AgentSnapshotClient>(httpClient =>
+            {
+                httpClient.BaseAddress =
+                    new Uri("https://status-api.kgivler.com/");
+
+                httpClient.Timeout =
+                    TimeSpan.FromSeconds(10);
+            });
+
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
         return builder.Build();
