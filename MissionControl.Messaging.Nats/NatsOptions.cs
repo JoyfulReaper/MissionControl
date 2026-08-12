936a33caf975fa12918f5a0f7f96b83db1f83ae0
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-12 13:13:04 -0400

    Add NATS connection options

---
+++ MissionControl.Messaging.Nats/NatsOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MissionControl.Messaging.Nats;
+
+public sealed class NatsOptions
+{
+    public const string SectionName = "Nats";
+    public string Url { get; init; } = "nats://localhost:4222";
+    public string StreamName { get; init; } = "MISSION_CONTROL_EVENTS";
+}
---
+++ MissionControl.Messaging.Nats/NatsServiceCollectionExtenstions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MissionControl.Messaging.Nats;
+
+public static class NatsServiceCollectionExtensions
+{
+    public static IServiceCollection AddNatsOptions(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddOptions<NatsOptions>()
+            .Bind(configuration.GetSection(NatsOptions.SectionName))
+            .Validate(
+                options => IsValidNatsUrl(options.Url),
+                "Nats:Url must be a valid NATS URL.")
+            .Validate(
+                options => !string.IsNullOrWhiteSpace(options.StreamName),
+                "Nats:StreamName is required.")
+            .ValidateOnStart();
+
+        return services;
+    }
+
+    private static bool IsValidNatsUrl(string url)
+    {
+        if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
+        {
+            return false;
+        }
+
+        return uri.Scheme is "nats" or "tls";
+    }
+}
