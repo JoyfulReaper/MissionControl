511e43abaa4f9db4640b136ef87f02d952128f9d
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-12 13:30:31 -0400

    Register shared NATS connection

--- MissionControl.Messaging.Nats/NatsOptions.cs
+++ MissionControl.Messaging.Nats/NatsOptions.cs
@@ -4,5 +4,6 @@ public sealed class NatsOptions
 {
     public const string SectionName = "Nats";
     public string Url { get; init; } = "nats://localhost:4222";
+    public string ClientName { get; init; } = string.Empty;
     public string StreamName { get; init; } = "MISSION_CONTROL_EVENTS";
 }
--- MissionControl.Messaging.Nats/NatsServiceCollectionExtensions.cs
+++ MissionControl.Messaging.Nats/NatsServiceCollectionExtensions.cs
@@ -1,10 +1,49 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Extensions.Microsoft.DependencyInjection;
+using NATS.Net;
 
 namespace MissionControl.Messaging.Nats;
 
 public static class NatsServiceCollectionExtensions
 {
+
+    public static IServiceCollection AddNatsConnection(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddNatsOptions(configuration);
+        services.AddNatsClient(nats =>
+        {
+            nats.ConfigureOptions(options =>
+            {
+                options.Configure<IOptions<NatsOptions>>(
+                    (natsOptionsBuilder, configuredOptions) =>
+                    {
+                        var configured = configuredOptions.Value;
+                        natsOptionsBuilder.Opts =
+                            natsOptionsBuilder.Opts with
+                            {
+                                Url = configured.Url,
+                                Name = configured.ClientName
+                            };
+                    });
+            });
+        });
+
+        services.AddSingleton<INatsJSContext>(serviceProvider =>
+        {
+            var client = serviceProvider.GetRequiredService<INatsClient>();
+
+            return client.CreateJetStreamContext();
+        });
+
+        return services;
+    }
+
     public static IServiceCollection AddNatsOptions(
         this IServiceCollection services,
         IConfiguration configuration)
@@ -18,6 +57,9 @@ public static class NatsServiceCollectionExtensions
             .Validate(
                 options => !string.IsNullOrWhiteSpace(options.StreamName),
                 "Nats:StreamName is required.")
+            .Validate(
+                options => !string.IsNullOrWhiteSpace(options.ClientName),
+                "Nats:ClientName is required.")
             .ValidateOnStart();
 
         return services;
