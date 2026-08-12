d3ac4f84d192bda85ffd0d2d8d534cc2cc41664f
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-12 13:19:47 -0400

    Define NATS event subject topology

---
+++ MissionControl.Messaging.Nats/NatsSubjects.cs
@@ -0,0 +1,23 @@
+﻿namespace MissionControl.Messaging.Nats;
+
+public static class NatsSubjects
+{
+    public const string EventPrefix = "events";
+    public const string AllEvents = EventPrefix + ".>";
+
+    public static string ForEventType(string eventType)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(eventType);
+
+        if (eventType.Contains('*') ||
+            eventType.Contains('>') ||
+            eventType.Any(char.IsWhiteSpace))
+        {
+            throw new ArgumentException(
+                "Event type contains characters that are invalid in a NATS subject.",
+                nameof(eventType));
+        }
+
+        return EventPrefix + "." + eventType;
+    }
+}
